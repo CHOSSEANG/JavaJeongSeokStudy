b151f072f43a75e7f15c43d37866b6c342739e2b
chosseang <jng00153@gmail.com>
2024-09-17 20:11:00 +0900

    ✨ [ch2] 특수문자 출력 예제 실습

---
+++ src/ch2/SpecialChar.java
@@ -0,0 +1,13 @@
+package ch2;
+
+public class SpecialChar {
+    public static void main(String[] args) {
+        char single = '\'';
+        String dblQuote = "\"Hello\"";
+        String root = "C:\\";
+
+        System.out.println(single);
+        System.out.println(dblQuote);
+        System.out.println(root);
+    }
+}
