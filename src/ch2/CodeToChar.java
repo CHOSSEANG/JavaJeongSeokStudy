dfa031b6d99b54bc79c22ff7ea9a7dcef42cd88a
chosseang <jng00153@gmail.com>
2024-09-17 20:10:40 +0900

    ✨ [ch2] 문자열 형변환 예제 실습

---
+++ src/ch2/CodeToChar.java
@@ -0,0 +1,11 @@
+package ch2;
+
+public class CodeToChar {
+    public static void main(String[] args) {
+        int code = 65;
+        char ch = (char) code;
+
+        System.out.println(code);
+        System.out.println(ch);
+    }
+}
