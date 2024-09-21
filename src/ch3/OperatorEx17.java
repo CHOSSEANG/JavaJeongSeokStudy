e7e588a31ead90975b383511d88db213ba4c0f03
chosseang <jng00153@gmail.com>
2024-09-21 23:48:34 +0900

    ✨ [ch3] 실수형 변수 소숫점 셋째 자리 출력 예제 실습

---
+++ src/ch3/OperatorEx17.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class OperatorEx17 {
+    public static void main(String[] args) {
+        float pi = 3.141592f;
+        float shortPi = (int) (pi * 1000) / 1000f;
+
+        System.out.println(shortPi);
+    }
+}
