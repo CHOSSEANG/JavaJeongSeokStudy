a8637c7e4de101e91f94dd8df29829cb64281423
chosseang <jng00153@gmail.com>
2024-09-22 00:05:42 +0900

    ✨ [ch3] 실수형 변수 math 라이브러리 예제 실습

---
+++ src/ch3/OperatorEx18.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class OperatorEx18 {
+    public static void main(String[] args) {
+        float pi = 3.141592f;
+        float shortPi = Math.round(pi * 1000) / 1000f;
+
+        System.out.println(shortPi);
+    }
+}
