2aede4b0f215eb38b2af02134d85de721d0621dd
chosseang <jng00153@gmail.com>
2024-09-21 15:52:03 +0900

    ✨ [ch3] long 형 변수 리터럴에 따른 연산 실습 예제

---
+++ src/ch3/OperatorEx11.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class OperatorEx11 {
+    public static void main(String[] args) {
+        long a = 1000000 * 1000000;
+        long b = 1000000 * 1000000L;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
