174d0f67fda6ed7938d4ea8f8c19a610a5b7b4e0
chosseang <jng00153@gmail.com>
2024-09-21 15:34:17 +0900

    ✨ [ch3] 비트전환 연산자 예제 실습

---
+++ src/ch3/OperatorEx5.java
@@ -0,0 +1,10 @@
+package ch3;
+
+public class OperatorEx5 {
+    public static void main(String[] args) {
+        byte b = 10;
+        System.out.println("b = " + b);
+        System.out.println("~b = " + ~b);
+        System.out.println("~b + 1 = " + (~b + 1));
+    }
+}
