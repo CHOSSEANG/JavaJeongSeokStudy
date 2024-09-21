545be298feb33e882150a78b1a2120367faf819c
chosseang <jng00153@gmail.com>
2024-09-21 15:43:23 +0900

    ✨ [ch3] 논리 연산자 예제 실습

---
+++ src/ch3/OperatorEx7.java
@@ -0,0 +1,12 @@
+package ch3;
+
+public class OperatorEx7 {
+    public static void main(String[] args) {
+        boolean power = false;
+        System.out.println(power);
+        power = !power; // power의 값이 false에서 true로 바뀜
+        System.out.println(power);
+        power = !power;// power의 값이 true에서 false로 바뀜
+        System.out.println(power);
+    }
+}
