2a7f975a5211440c9da8d61818c5577f141046d1
chosseang <jng00153@gmail.com>
2024-09-23 05:26:41 +0900

    ✨ [ch4] if 조건문 예제 실습

---
+++ src/ch4/FlowEx1.java
@@ -0,0 +1,10 @@
+package ch4;
+
+public class FlowEx1 {
+    public static void main(String[] args) {
+        int visitCount = 0;
+        if (visitCount < 1) {
+            System.out.println("처음 오셨군요. 방문해주셔서 감사합니다.");
+        }
+    }
+}
---
+++ src/ch4/FlowEx2.java
@@ -0,0 +1,13 @@
+package ch4;
+
+public class FlowEx2 {
+    public static void main(String[] args) {
+        int visitCount = 5;
+        if (visitCount < 1) {
+            System.out.println("처음 오셨군요. 방문해주셔서 감사합니다.");
+        } else{
+            System.out.println("다시 방문해 주셔서 감사합니다.");
+        }
+        System.out.println("방문횟수는 " + ++visitCount + "번 입니다.");
+    }
+}
