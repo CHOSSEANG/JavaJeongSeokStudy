e49f939b2f97dfeea479f6d7642eac5ecb955523
ssang-labsd <ssang@labsd.net>
2024-09-26 02:20:26 +0900

    ✨ [ch4] while 반복문 예제 실습

---
+++ src/ch4/FlowEx21.java
@@ -0,0 +1,10 @@
+package ch4;
+
+public class FlowEx21 {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i >= 0) {
+            System.out.println(i--);
+        }
+    }
+}
---
+++ src/ch4/FlowEx23.java
@@ -0,0 +1,13 @@
+package ch4;
+
+public class FlowEx23 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 0;
+
+        while (sum + i <= 100) {
+            sum += ++i;
+            System.out.println(i + " - " + sum);
+        }
+    }
+}
