ab596f5b07d97c26785b4a49ff2fa173c82c22cc
ssang-labsd <ssang@labsd.net>
2024-09-26 02:36:44 +0900

    ✨ [ch4] continue문 예제 실습

---
+++ src/ch4/FlowEx26.java
@@ -0,0 +1,11 @@
+package ch4;
+
+public class FlowEx26 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            if (i % 3 == 0)
+                continue;
+            System.out.println(i);
+        }
+    }
+}
