df00b0e183792bf043e0bcce687c8e40e3d0c250
ssang-labsd <ssang@labsd.net>
2024-09-28 15:20:31 +0900

    ✨ [ch5] 예제 풀이 실습

---
+++ src/ch5/ArrayEx6.java
@@ -0,0 +1,19 @@
+package ch5;
+
+public class ArrayEx6 {
+    public static void main(String[] args) {
+        int[] number = new int[10];
+        int[] counter = new int[10];
+
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i]=(int)(Math.random()*10));
+        }
+        System.out.println();
+        for (int i = 0; i < number.length; i++) {
+            counter[number[i]]++;
+        }
+        for (int i = 0; i < counter.length; i++) {
+            System.out.println(i+"의 개수 : "+counter[i]);
+        }
+    }
+}
