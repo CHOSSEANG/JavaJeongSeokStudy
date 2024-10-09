f3902710a0016af9f2a9d916361ac7f967aa0114
chosseang <jng00153@gmail.com>
2024-10-09 23:43:17 +0900

    ✨ [ch14] IO 예제 실습

---
+++ src/ch14/FileReaderEx1.java
@@ -0,0 +1,31 @@
+package ch14;
+
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+class FileReaderEx1 {
+    public static void main(String[] args) {
+        try {
+            String fileName = "test.txt";
+            FileInputStream fis = new FileInputStream(fileName);
+            FileReader fr = new FileReader(fileName);
+
+            int data = 0;
+
+            while ((data = fis.read()) != -1) {
+                System.out.print((char) data);
+            }
+            System.out.println();
+            fis.close();
+
+            while ((data = fr.read()) != -1) {
+                System.out.print((char) data);
+            }
+            System.out.println();
+            fr.close();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
